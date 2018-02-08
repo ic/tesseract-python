36a5d7fe659613e938028d4067035261bdc317a0
Eric Platon <zaraki@gmx.com>
2018-02-08 18:34:43 +0900

    Fix path to the Tesseract embedded binary.

--- tesseract_python/__init__.py
+++ tesseract_python/__init__.py
@@ -8,7 +8,7 @@ except ImportError:
 
 pytesseract.pytesseract.tesseract_cmd =  os.sep.join([
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-    'tesseract',
+    'tesseract_python',
     'bin',
     'tesseract'
 ])
