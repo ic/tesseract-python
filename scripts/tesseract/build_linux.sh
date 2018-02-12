e969a378cfeacc1d63679cb9e06e42fcb53e9807
Eric Platon <zaraki@gmx.com>
2018-02-12 16:02:27 +0900

    Add missing declaration.

--- scripts/tesseract/build_linux.sh
+++ scripts/tesseract/build_linux.sh
@@ -9,6 +9,10 @@ WDIR="$(get_realpath $(dirname $(dirname $(dirname "$0"))))"
 PREFIX=$WDIR/tesseract_python/tesseract
 mkdir -p $PREFIX
 
+LEPTONICA_HOME=$WDIR/tesseract_python/leptonica
+
+echo "Will build with Leptonica at $LEPTONICA_HOME"
+
 pushd tesseract-src
 ./autogen.sh
 LIBLEPT_HEADERSDIR=$LEPTONICA_HOME/include ./configure \
