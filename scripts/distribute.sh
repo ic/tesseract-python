6b8733d58a2897dc171792585cbf7c318449e03a
Eric Platon <zaraki@gmx.com>
2018-02-13 12:19:37 +0900

    Change Twine command to reflect upcoming wheel generation.

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -7,7 +7,7 @@ source venv/bin/activate
 if [ -n "$TRAVIS_TAG" ]
 then
   ls -l $TRAVIS_BUILD_DIR
-  twine upload -u $USER -p $PASS --skip-existing $TRAVIS_BUILD_DIR/wheelhouse/tesseract*;
+  twine upload -u $PYPI_USER -p $PYPI_PASS --skip-existing $TRAVIS_BUILD_DIR/dist/tesseract*
 else
   echo "Travis tag missing. Aborting distribution.";
 fi
