10c89b175bf4d99bbc091ba1d6d50a11271bfff1
Eric Platon <zaraki@gmx.com>
2018-02-13 16:29:29 +0900

    Focus on Linux for TravisCI.

--- config.sh
+++ config.sh
@@ -1,3 +1,10 @@
+function pre_build {
+  pwd
+  ls -l /io
+  # Make Tesseract and Leptonica
+  make
+}
+
 function run_tests {
   python --version
   python -c "import tesseract_python; from PIL import Image; tesseract_python.pytesseract.image_to_string(Image.open('test/data/img_with_text.png'))"
