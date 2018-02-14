f654f38c6f2a326a81c30ef70dd2116ee92ea9d7
Eric Platon <zaraki@gmx.com>
2018-02-14 17:15:38 +0900

    Install or upgrade twine on distribute.

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -6,7 +6,7 @@ source venv/bin/activate
 
 if [ -n "$TRAVIS_TAG" ]
 then
-  ls -l $TRAVIS_BUILD_DIR
+  pip install --upgrade twine
   twine upload -u $PYPI_USER -p $PYPI_PASS --skip-existing ${TRAVIS_BUILD_DIR}/wheelhouse/tesseract*
 else
   echo "Travis tag missing. Aborting distribution.";
