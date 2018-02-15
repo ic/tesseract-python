d26e3ac8abed13fe2e4cf372121fe0f83d9a6288
Eric Platon <zaraki@gmx.com>
2018-02-15 15:42:19 +0900

    Need to add path to liblept too...

--- tesseract_python/__init__.py
+++ tesseract_python/__init__.py
@@ -8,7 +8,10 @@ os.environ['TESSDATA_PREFIX'] = os.sep.join([
     'tessdata'
 ])
 
-ld_library_paths = [ os.sep.join([os.path.dirname(__file__), 'tesseract', 'lib']) ]
+ld_library_paths = [
+  os.sep.join([os.path.dirname(__file__), 'tesseract', 'lib']),
+  os.sep.join([os.path.dirname(__file__), 'leptonica', 'lib']),
+]
 if 'LD_LIBRARY_PATH' in os.environ.keys():
   ld_library_paths.append(os.environ['LD_LIBRARY_PATH'])
 os.environ['LD_LIBRARY_PATH'] = ':'.join(ld_library_paths)
