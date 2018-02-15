04b62b83eee12ca213c0ca029b1af69d8734e941
Eric Platon <zaraki@gmx.com>
2018-02-15 13:53:51 +0900

    Specify where to find libtesseract when deployed.

--- tesseract_python/__init__.py
+++ tesseract_python/__init__.py
@@ -8,6 +8,11 @@ os.environ['TESSDATA_PREFIX'] = os.sep.join([
     'tessdata'
 ])
 
+os.environ['LD_LIBRARY_PATH'] = ':'.join([
+  os.sep.join([os.path.dirname(__file__), 'tesseract', 'lib'])
+  os.environ['LD_LIBRARY_PATH']
+])
+
 pytesseract.pytesseract.tesseract_cmd = os.sep.join([
     os.path.dirname(os.path.abspath(__file__)),
     'tesseract',
