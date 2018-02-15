86c001504f5afff6b8dd130a8f08308b83681f00
Eric Platon <zaraki@gmx.com>
2018-02-15 15:04:42 +0900

    Fix syntax error.

--- tesseract_python/__init__.py
+++ tesseract_python/__init__.py
@@ -8,10 +8,10 @@ os.environ['TESSDATA_PREFIX'] = os.sep.join([
     'tessdata'
 ])
 
-os.environ['LD_LIBRARY_PATH'] = ':'.join([
-  os.sep.join([os.path.dirname(__file__), 'tesseract', 'lib'])
-  os.environ['LD_LIBRARY_PATH']
-])
+ld_library_paths = [ os.sep.join([os.path.dirname(__file__), 'tesseract', 'lib']) ]
+if 'LD_LIBRARY_PATH' in os.environ.keys():
+  ld_library_paths.append(os.environ['LD_LIBRARY_PATH'])
+os.environ['LD_LIBRARY_PATH'] = ':'.join(ld_library_paths)
 
 pytesseract.pytesseract.tesseract_cmd = os.sep.join([
     os.path.dirname(os.path.abspath(__file__)),
