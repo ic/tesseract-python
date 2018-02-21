c1fc6d80accdceab8c3a9bd03dd45d7fd954dbe6
Eric Platon <zaraki@gmx.com>
2018-02-21 15:47:12 +0900

    WIP Embed custom build for libjpeg.
    This is useful for Linux distributions without libjpeg, e.g. AWS Lambda instances.

--- scripts/leptonica/dep_linux.sh
+++ scripts/leptonica/dep_linux.sh
@@ -13,8 +13,9 @@ then
     libtool \
     autoconf-archive \
     pkg-config \
+    nasm \
+    git \
     libpng-dev \
-    libjpeg8-dev \
     libtiff5-dev \
     zlib1g-dev
 elif [ -x '/usr/bin/yum' ]
@@ -27,10 +28,36 @@ then
     automake \
     libtool \
     pkgconfig \
+    nasm \
+    git \
     libpng-devel \
-    libjpeg-devel \
     libtiff-devel \
     zlib-devel
 else
   echo "Could not find Apt or Yum in standard locations, aborting."
 fi
+
+
+source scripts/common.sh
+WDIR="$(get_realpath $(dirname $(dirname $(dirname "$0"))))"
+
+
+LIB_DIR=$WDIR/tesseract_python/lib
+mkdir -p $LIB_DIR
+
+SRC_DIR=$WDIR/tmp/src
+mkdir -p $SRC_DIR
+
+BUILD_DIR=$WDIR/tmp/build
+mkdir -p $BUILD_DIR
+
+# libjpeg
+LIBJPEG=$SRC_DIR/libjpeg
+git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git $LIBJPEG
+git checkout 1.5.3
+pushd $LIBJPEG
+autoreconf -fiv
+./configure
+make
+cp .libs/libjpeg.so.62.2.0 .libs/libjpeg.so $LIB_DIR
+popd
