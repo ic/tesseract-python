5fe0f438191a08a08af27661b0ae2555b2627b1f
Eric Platon <zaraki@gmx.com>
2018-02-23 11:12:41 +0900

    Build Leptonica with the local libjpeg.

--- scripts/leptonica/build_linux.sh
+++ scripts/leptonica/build_linux.sh
@@ -9,9 +9,15 @@ WDIR="$(get_realpath $(dirname $(dirname $(dirname "$0"))))"
 PREFIX=$WDIR/tesseract_python/leptonica
 mkdir -p $PREFIX
 
+SELF_LIB_DIR=$WDIR/tmp/build/lib
+SELF_INCLUDE_DIR=$WDIR/tmp/build/include
+
 pushd leptonica-src
 ACLOCAL_PATH=/usr/share/aclocal:$ACLOCAL_PATH ./autobuild
-./configure --prefix=$PREFIX
+./configure \
+    --prefix=$PREFIX \
+    --with-extra-includes=$SELF_INCLUDE_DIR/libjpeg \
+    LDFLAGS="-static -L$SELF_LIB_DIR"
 make
 make install
 popd
